7efea9c10e11e0f59743c7c6878fb6d51e9193e6
LastWord7722 <lastwordmark@gmail.com>
2025-07-01 21:44:51 +0300

    add move Queen

--- WinFormsApp1/src/Entities/Figures/Queen.cs
+++ WinFormsApp1/src/Entities/Figures/Queen.cs
@@ -8,9 +8,148 @@ public class Queen : BaseFigure
     public Queen(FigureColor color, Position position) : base(color, position)
     {
     }
+    private List<Position> ProcessDiagonal(Chessboard.Chessboard chessboard, bool isLeft = true, bool isTop = true)
+    {
+        List<Position> moves = new List<Position>();
+        char[] columnList = chessboard.GetListColumns();
+        int currentRow = Position.GetRow();
+        char? currentColumn = Position.GetColumn();
+        
+        while (true)
+        {
+         
+            if (currentRow == -1 || currentColumn == null)
+            {
+                break;
+            }
 
+            currentRow = isTop ? GetNextRow(currentRow) : GetPreviousRow(currentRow);
+            currentColumn = isLeft 
+                ? GetLeftColumn(currentColumn.Value, columnList) 
+                : GetRightColumn(currentColumn.Value, columnList);
+            
+            
+            if (currentRow == -1 || currentColumn == null)
+            {
+                break;
+            }
+
+            Position currentPosition = Position.Make(currentRow, currentColumn.Value);
+            if (chessboard.HasUnionFigureByPosition(currentPosition, Color))
+            {
+                break;
+            }
+
+            if (chessboard.HasEnemyFigureByPosition(currentPosition, GetEnemyColor()))
+            {
+                moves.Add(currentPosition);
+                break;
+            }
+
+            moves.Add(currentPosition);
+        }
+
+        return moves;
+    }
+  private List<Position> GetAvailableMovesVertical(Chessboard.Chessboard chessboard, bool isLeft = true)
+    {
+        char[] columnList = chessboard.GetListColumns();
+        List<Position> moves = new List<Position>();
+        char? currentColumn = Position.GetColumn();
+        while (true)
+        {
+            if (currentColumn == null)
+            {
+                break;
+            }
+
+            currentColumn = isLeft
+                ? GetLeftColumn(currentColumn.Value, columnList)
+                : GetRightColumn(currentColumn.Value, columnList);
+
+            if (currentColumn == null)
+            {
+                break;
+            }
+
+            Position currentPosition = Position.Make(currentColumn.Value, Position.GetRow());
+            if (chessboard.HasUnionFigureByPosition(currentPosition, Color))
+            {
+                break;
+            }
+
+            if (chessboard.HasEnemyFigureByPosition(currentPosition, GetEnemyColor()))
+            {
+                moves.Add(currentPosition);
+                break;
+            }
+
+            moves.Add(currentPosition);
+        }
+
+        return moves;
+    }
+
+    private List<Position> GetAvailableMovesHorizontal(Chessboard.Chessboard chessboard, bool isTop = true)
+    {
+        List<Position> moves = new List<Position>();
+        int currentRow = Position.GetRow();
+
+        while (true)
+        {
+            if (currentRow == -1)
+            {
+                break;
+            }
+
+            currentRow = isTop
+                ? GetNextRow(currentRow)
+                : GetPreviousRow(currentRow);
+
+            if (currentRow == -1)
+            {
+                break;
+            }
+
+            Position currentPosition = Position.Make(currentRow, Position.GetColumn());
+            if (chessboard.HasUnionFigureByPosition(currentPosition, Color))
+            {
+                break;
+            }
+
+            if (chessboard.HasEnemyFigureByPosition(currentPosition, GetEnemyColor()))
+            {
+                moves.Add(currentPosition);
+                break;
+            }
+
+            moves.Add(currentPosition);
+        }
+
+        return moves;
+    }
     public override List<Position> GetAvailableMoves(Chessboard.Chessboard chessboard)
     {
-        throw new NotImplementedException();
+        List<Position> moves = new List<Position>();
+        moves.AddRange(GetAvailableMovesVertical(chessboard));
+        moves.AddRange(GetAvailableMovesVertical(chessboard, false));
+        moves.AddRange(GetAvailableMovesHorizontal(chessboard));
+        moves.AddRange(GetAvailableMovesHorizontal(chessboard, false));
+        
+        bool[,] matrix = new bool[,]
+        {
+            { true, false },
+            { false, true },
+            { false, false },
+            { true, true }
+        };
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            moves.AddRange(
+                ProcessDiagonal(chessboard, matrix[i,0], matrix[i,1])
+            );
+        }
+
+        return moves;
     }
 }
